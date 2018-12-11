883e3702b5fbc8ed83969778a0fc27f3099c0e95
Sermar De Sousa <sermar.sousa@connectcom.univesp.br>
2018-12-11 11:18:20 -0200

    Fixes config database archive.

--- config/database.php
+++ config/database.php
@@ -39,15 +39,15 @@ return [
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
-
+        
         // Sou Auditoria
-        'mysql_sa' => [
+        'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST_SA', '104.196.22.172'),
-            'port' => env('DB_PORT_SA', '3306'),
-            'database' => env('DB_DATABASE_SA', 'forge'),
-            'username' => env('DB_DATABASE_SA', 'forge'),
-            'password' => env('DB_DATABASE_SA', ''),
+            'host' => env('DB_HOST', '104.196.22.172'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
@@ -58,13 +58,13 @@ return [
         ],
 
         // Sou Authentication
-        'mysql' => [
+        'mysql_sa' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '104.196.22.172'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => env('DB_HOST_SA', '104.196.22.172'),
+            'port' => env('DB_PORT_SA', '3306'),
+            'database' => env('DB_DATABASE_SA', 'forge'),
+            'username' => env('DB_DATABASE_SA', 'forge'),
+            'password' => env('DB_DATABASE_SA', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
