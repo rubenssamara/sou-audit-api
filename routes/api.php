02907854d2a202546693439e1658971458b89120
lucas castro <lucas.castro1993@hotmail.com>
2018-12-12 09:35:05 -0200

    Adds routes end StudentController

--- routes/api.php
+++ routes/api.php
@@ -2,17 +2,50 @@
 
 use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+// EDUARDO
+Route::resources([
+    'course' => 'AuditCourseController',   
+]);
+
+Route::get('filter/{filter}','AuditCourseController@getCourseOrInstitution');
+Route::get('legislation','AuditCourseController@Legislation');
+Route::get('urgency','AuditCourseController@UrgencyDeadline');
+Route::get('students_enabled','AuditCourseController@EnabledStudents');
+Route::get('report/pdf','AuditCourseController@ReportPdf');
+Route::get('report/excel','AuditCourseController@ReportExcel');
+
+// LUCAS
+Route::resources([
+    'student' => 'StudentController',
+   
+]);
+
+Route::get('student/','StudentController@show'); //lista todos alunos
+Route::get('student-list/{filter}','StudentController@StudentResearch'); // faz pesquisa na lista de aluno
+Route::get('student-report/pdf','StudentController@ReportPdf'); //rotas de Relatorios
+Route::get('report/excel','StudentController@ReportExcel');
+
+Route::get('/rejected','StudentController@RetainedStudent'); // Alunos rejeitados
+Route::get('rejected/{id}','StudentController@RetainedStudentList');//pesquisa alunos rejeitados
+Route::get('rejected-report','StudentController@RejectedReportPdf'); //rotas de Relatorios
+Route::get('rejected-report/excel','StudentController@RejectedReportExcel');
+
+
+
+
+// RUBENS
+Route::resources([
+    
+   
+]);
+// SERMAR
+Route::resources([
+    
+   
+]);
+
